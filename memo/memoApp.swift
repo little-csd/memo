d197c79e58738859e14e02c8bfb396531b0e74b6
stevecai <stevecai@tencent.com>
2021-11-20 23:24:21 +0800

    Initial Commit

---
+++ memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by stevecai on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
